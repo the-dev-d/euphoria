154de7dd260de1191dc9c18f870750be45dd0358
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-07-23 16:12:47 +0530

    merge complete

--- src/lib/components/LandingPage/DevD-Parallax.svelte
+++ src/lib/components/LandingPage/DevD-Parallax.svelte
@@ -1,93 +1,55 @@
 <script>
-	import Parallax from '$lib/components/Parallax.svelte';
+
+import Parallax from "../Parallax.svelte";
+
 </script>
 
-<Parallax>
-	<section class="min-h-[740px] relative w-full max-h-[1080px] h-[100svh] mt-10 mb-10 parallax-scene overflow-hidden">
-		<div data-depth="0.8" class="absolute top-0 right-0 w-full h-full">
-			<img
-				class=" object-contain w-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 anim-studder"
-				src="/images/main/content-glitch.png"
-				alt=""
-			/>
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+
+<section class=" relative w-full h-[100svh] mt-8 mb-8">
+
+	<Parallax>
+	<div class=" absolute left-0 top-0 w-full h-full parallax-scene overflow-hidden">
+		<img data-caliberate-x="true" data-scalar-y="0.5" data-depth="0.4" class="absolute top-0 left-0 w-full h-full max-h-full object-cover object-bottom anim-studder" src='/images/main/content-glitch.png' alt="">
+		<div data-depth="0.25" class="w-full h-[25%] grid content-end">
+			<div class="font-valorant text-center text-9xl from-red-500 to-red-400 bg-gradient-to-br bg-clip-text text-transparent">SOUTH INDIAS BIGGEST</div>
 		</div>
-		<div data-depth="0.5" class=" absolute top-0 right-0 w-full h-full">
-			<img
-				class=" object-contain w-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 anim-studder"
-				src="/images/main/glitch.png"
-				alt=""
-			/>
+		<img data-caliberate-x="true" data-scalar-y="0.5" data-depth="0.35" id="glitch" class="absolute top-0 left-0 w-full h-full max-h-full object-cover object-bottom anim-studder" src='/images/main/glitch.png' alt="">
+		<img data-caliberate-x="true" data-scalar-y="0.5" data-depth="0.0" id="floor" class="absolute top-0 left-0 w-full h-full max-h-full object-cover object-bottom" src='/images/main/floor.png' alt="">
+		<img data-caliberate-x="true" data-scalar-y="0.5" data-depth="0.15" id="agents" class="absolute top-0 left-0 w-full h-full max-h-full object-cover object-bottom" src='/images/main/agents-3.png' alt="">
+		<img data-caliberate-x="true" data-scalar-y="0.5" data-depth="0.2" id="agents" class="absolute top-0 left-0 w-full h-full max-h-full object-cover object-bottom" src='/images/main/agents-2.png' alt="">
+		<img data-caliberate-x="true" data-scalar-y="0.5" data-depth="0.1" id="agents" class="absolute top-0 left-0 w-full h-full max-h-full object-cover object-bottom" src='/images/main/agents.png' alt="">
+		<div data-depth="0.08" class="w-full h-full grid content-end">
+			<div class="font-valorant text-center text-black stroke-transparent stroke-[3rem] text-9xl from-green-500 to-green-400 bg-gradient-to-br bg-clip-text">LAN TOURNAMENT lore</div>
 		</div>
-		<div data-depth="0.3" class=" absolute top-0 right-0 w-full h-full">
-			<img
-				class=" object-contain w-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-				src="/images/main/logo.png"
-				alt=""
-			/>
-		</div>
-		<div data-depth="0.1" class=" absolute top-0 right-0 w-full h-full">
-			<img
-				class=" object-contain w-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-				src="/images/main/floor.png"
-				alt=""
-			/>
-		</div>
-		<div data-depth="0.2" class=" absolute top-0 right-0 w-full h-full">
-			<img
-				class=" object-contain w-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-				src="/images/main/agents.png"
-				alt=""
-			/>
-		</div>
-		<section
-			class="absolute top-0 left-0 h-full w-full"
-		>
-			<div class="absolute top-1/2 left-0 gl:left-10 transform -translate-x-0 -translate-y-1/2 flex flex-col lg:justify-center text-center lg:text-left">
-				<h1 class="animate-fly-left font-equinox uppercase font-bold text-2xl">
-					Rajagairi College of Social Sciences
-					<br /><span class="text-base">In collaboration with</span><span
-						class="tracking-widest ml-2">AKEF</span
-					>
-				</h1>
-				<div
-					class=" animate-fly-left-slow bg-gradient-to-br from-red-500 to-red-300 bg-clip-text text-transparent flex flex-col justify-center items-center"
-				>
-					<h1 class="text-6xl sm:text-8xl font-valorant">EUPHORIA</h1>
-					<h4 class="flex justify-between font-equinox text-xl w-full font-semibold">
-						<span>R</span>
-						<span>E</span>
-						<span>S</span>
-						<span>P</span>
-						<span>A</span>
-						<span>W</span>
-						<span>N</span>
-					</h4>
-				</div>
-			</div>
-		</section>
-	</section>
-</Parallax>
+	</div>
+	</Parallax>
+</section>
 
 <style lang="postcss">
+
+	.text-border {
+		text-shadow: 0px 0px 0px 1px white;
+	}
+	.container-inline {
+		container-type: inline-size;
+	}
 	@keyframes glow {
-		0%,
-		20%,
-		40%,
-		55%,
-		90%,
-		100% {
+		0%, 20%, 40%, 55%, 90%, 100% {
 			filter: brightness(100%);
 			opacity: 0.6;
 		}
 
-		10%,
-		45%,
-		92% {
+		10%, 45%, 92% {
 			filter: brightness(300%);
 			opacity: 1;
 		}
 	}
 	.anim-studder {
-		animation: glow 3s linear infinite;
+		animation: glow 3s linear infinite ;
 	}
-</style>
+</style> 
