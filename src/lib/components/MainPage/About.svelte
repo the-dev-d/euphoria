eaf40aea372f5fa465229adce97fcf20bc4ca8a7
the-dev-d <dhaivathl@gmail.com>
2024-07-24 23:10:12 +0530

    resolution fix

--- src/lib/components/MainPage/About.svelte
+++ src/lib/components/MainPage/About.svelte
@@ -4,7 +4,7 @@
 <section id="about" class="relative mb-10 mt-10 min-h-[740px] w-full p-5 text-center gl:p-10">
 	<h1 class=" relative font-valorant text-6xl uppercase text-white gl:text-6xl">ABOUT</h1>
 	<div class="relative grid h-full w-full grid-rows-2 place-items-center gap-12 gl:gap-0">
-		<div class="flex h-full w-full items-center">
+		<div class="flex h-full w-full items-center ">
 			<figure class="relative h-fit place-content-center">
 				<img
 					class="aspect-square w-[10rem] object-cover gl:w-[20rem]"
@@ -12,7 +12,7 @@
 					alt="euph"
 				/>
 			</figure>
-			<article class="relative h-full w-full flex-1 overflow-hidden text-left gl:w-full gl:p-10">
+			<article class="relative h-fit w-full flex-1 overflow-hidden text-left gl:w-full gl:p-10">
 				<p class=" text-xm relative block text-justify font-sans gl:text-xl">
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit provident autem
 					assumenda nisi voluptatibus, ea aperiam hic molestias vero iste minima adipisci excepturi
@@ -26,7 +26,7 @@
 			</article>
 		</div>
 		<div class="flex h-full w-full items-center">
-			<article class=" relative h-full w-full flex-1 overflow-hidden text-left gl:w-full gl:p-10">
+			<article class=" relative h-fit w-full flex-1 overflow-hidden text-left gl:w-full gl:p-10">
 				<p class=" text-xm w-fullfont-sans relative block text-justify gl:text-xl">
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit provident autem
 					assumenda nisi voluptatibus, ea aperiam hic molestias vero iste minima adipisci excepturi
@@ -38,7 +38,7 @@
 					voluptatibus quae. Amet, id ipsum? Unde, quia ea.
 				</p>
 			</article>
-			<figure class="relative h-fit place-content-center gl:w-1/4">
+			<figure class="relative h-fit place-content-center">
 				<img src="/images/logos/RcssLogo.png" alt="euph" class="aspect-square w-[10rem] object-cover gl:w-[20rem]" />
 			</figure>
 		</div>
