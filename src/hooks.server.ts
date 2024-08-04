70fef360b7b5860b27c0f194fa487abe37f31ac2
the-dev-d <dhaivathl@gmail.com>
2024-08-05 01:29:48 +0530

    asset compression

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,7 +7,6 @@ export const handle: Handle = async ({event, resolve}) => {
     const {cookies} = event;
     const token = cookies.get('session');
 
-    console.log("Hooking");
     if(token) {
         const [b64, hash] = token.split('.');
         const email = atob(b64);
